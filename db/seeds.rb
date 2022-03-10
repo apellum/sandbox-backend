ca00d3d96d603e3a2c1bb8951657f0dc73fd7e30
Andrew Pellum <apellum19@gmail.com>
2022-03-10 10:52:58 -0700

    Added initial setup

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+drew = User.create(name: "Drew Pellum", description: "Likes to code.", profile_picture: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.gq.com%2Fstory%2Ftravis-rice-snowboarding-diet&psig=AOvVaw0lKWC_EeKkGBZHngIt427N&ust=1646973275781000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCNDyj9vbuvYCFQAAAAAdAAAAABAD");
+tony = User.create(name: "Tony Hawk", description: "Brone my femur..", profile_picture: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.nbcsandiego.com%2Fnews%2Fsports%2Fgnarly-dude-skateboarding-legend-tony-hawk-suffers-nasty-broken-leg%2F2889999%2F&psig=AOvVaw2fUHl6VO4FkSnVKd6LkpcX&ust=1646973381742000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKD1pI3cuvYCFQAAAAAdAAAAABAD");
